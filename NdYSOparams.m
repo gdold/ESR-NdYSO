2df770680731d1c63a0cda6b8a6d5f9ab026b8c7
Gavin Dold <gdold42@gmail.com>
2016-11-02 18:10:58 +0000

    More functionality in eigfields solver

--- NdYSOparams.m
+++ NdYSOparams.m
@@ -8,6 +8,8 @@ function Sys = NdYSOparams(Sys,param)
 
 switch param
     case 'Maier-FlaigTensor'
+        % Site 1 is occupied to a much higher degree
+        % than site 2 for Nd:YSO
         Sys.g = [1.30 0.62 0.22;
                  0.62 -2.07 1.62;
                  0.22 1.62 -2.86];
@@ -20,7 +22,20 @@ switch param
         if isfield(Sys,'AFrame')
             Sys = rmfield(Sys,'AFrame');
         end
-            
+    
+    case 'Maier-FlaigGavin'
+        % Although Q*D*Q' gives the same tensor as above,
+        % The results from these don't match M-Ftensor
+        Sys.g = [-4.1407 1.4786 -0.9679];
+        Sys.A = [-809.0 -0.183 -495.5];
+        
+        gFrame_zyz = [0.1708 0.9153 1.2319];
+        AFrame_zyz = [-0.2127 0.9474 1.9135];
+        
+        Sys.gFrame = gFrame_zyz;
+        Sys.AFrame = AFrame_zyz;      
+        
+        
     case 'Maier-FlaigPrincipal'
         Sys.g = [-0.96 1.48 -4.14];
         Sys.A = [-0.146 -495.5 -808.0]; %MHz
@@ -41,13 +56,15 @@ switch param
         %gFrame_zxz = [192 39 183]*(pi/180); % Original euler convention
         %AFrame_zxz = [154 34 200]*(pi/180); % different to easyspin
         
-        gFrame_zyz = [1.7802 -0.6807 -1.5184]; % Converted to szyz
+        gFrame_zyz = [1.7802 -0.6807 -1.5184]; % Converted szxz->szyz
         AFrame_zyz = [1.1170 -0.5934 -1.2217]; % using euler.py
         
-        %gFrame_zyz = [-1.3614 -0.6807 1.6232]; % Converted to rzyz
+        %gFrame_zyz = [-1.5184 -0.6807 1.7802]; % Converted szxz->rzyz
+        %AFrame_zyz = [-1.2217 -0.5934 1.1170]; % using euler.py
+       
+        %gFrame_zyz = [-1.3614 -0.6807 1.6232]; % Converted rzxz->rzyz
         %AFrame_zyz = [-2.0258 -0.5934 1.9199]; % using euler.py
         
         Sys.gFrame = gFrame_zyz;
         Sys.AFrame = AFrame_zyz;
 end
-        
--- bRotation_eigfields.m
+++ bRotation_eigfields.m
@@ -1,6 +1,4 @@
 %% Spin system %%
-I_nuc = 3.5;
-
 Sys = struct();
 Sys.Nucs = '145Nd';
 %Sys.I = 3.5; %Specified by Nucs(?)
@@ -12,6 +10,7 @@ Sys.S = 0.5;
 % 'Maier-FlaigTensor'
 % 'Maier-FlaigPrincipal' % gives vastly different results to other two
 % 'Wolfowicz' - corrected Euler convention
+%parameter_source = 'Maier-FlaigPrincipal';
 parameter_source = 'Wolfowicz';
 Sys = NdYSOparams(Sys,parameter_source); % Appends chosen parameters to Sys
 
@@ -29,10 +28,14 @@ Exp.CrystalSymmetry = 'C2h'; %monoclinic C^6_2h spacegroup
 
 %% Crystal rotation %%
 % Calculate euler angles given xyz rotations
+%phi = 69.83;
+%theta = 3.75;
+%rho = 0;
 phi = 69.83;
-theta = 3.75;
-rho = 0;
-cryst_rot = eulang( rotz(phi) * roty(theta) * rotx(rho) );
+theta = 86;
+rho = 86;
+cryst_ang = [phi, theta, rho];
+cryst_rot = eulang( rotz(cryst_ang(1)) * roty(cryst_ang(2)) * rotx(cryst_ang(3)) );
 
 
 
@@ -45,22 +48,30 @@ Opt = struct();
 % --- Eigenfields ---
 %out = eigfields(Sys,Exp,Opt);
 
-num_of_points = 361;
+num_of_points = 3;
 angles = linspace(0,360,num_of_points)';
 x = [];
 y = [];
+angle_choice = 1; % phi, theta, rho
 
 for n = 1:length(angles)
-    n
-    rho = angles(n);
-    Exp.CrystalOrientation = eulang( rotz(phi) * roty(theta) * rotx(rho) );
+    disp(n);
+    cryst_ang(angle_choice) = angles(n);
+    Exp.CrystalOrientation = eulang( rotz(cryst_ang(1)) * roty(cryst_ang(2)) * rotx(cryst_ang(3)) );
     out = eigfields(Sys,Exp,Opt);
     fields = out{1}';
     %fields = fields((length(fields)-16):length(fields)); % exclude all but 16 strongest transitions
-    x_temp = linspace(rho,rho,length(fields));
+    %x_temp = linspace(rho,rho,length(fields));
+    x_temp = linspace(cryst_ang(angle_choice),cryst_ang(angle_choice),length(fields));
     
     x = [x x_temp];
     y = [y fields];
 end
 
 scatter(x,y,'.')
+xaxis_texts = {'Phi','Theta','Rho'};
+xlabel(xaxis_texts(angle_choice))
+ylabel('B (mT)')
+text_label = {['Source: ',parameter_source],
+              ['Phi: ',num2str(phi),' Theta: ',num2str(theta),' Rho: ',num2str(rho)]};
+annotation('textbox',[.2 .5 .3 .3],'string',text_label,'FitBoxToText','on');
