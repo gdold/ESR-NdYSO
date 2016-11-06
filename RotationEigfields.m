1567149e39c36f3dd2268567805d1cb397c0ff1b
Gavin Dold <gdold42@gmail.com>
2016-11-06 18:29:23 +0000

    Method now works for rotating in both lab and crystal frames. Need to check direction of rotations and order of operations.

--- RotationEigfields.m
+++ RotationEigfields.m
@@ -42,8 +42,8 @@ init_mag_vect = [-0.156794;0.987480;-0.017280];
 % init_mag_vect = [0.99998808;-0.004875;-2.358e-04];
 
 % The initial rotation from the crystal frame to the lab frame is then
-y_axis_in_cryst = cross(rot_axis,init_mag_vect); % true if rot,mag orthogonal
-init_rotm = [init_mag_vect,y_axis_in_cryst,rot_axis]'; % orthonormal?
+y_axis_in_cryst = cross(init_mag_vect,rot_axis); % true if rot,mag orthogonal
+init_rotm = [rot_axis,y_axis_in_cryst,init_mag_vect]'; % orthonormal?
 
 
 % Want a threshold intensity below which eigfields will ignore transition?
@@ -57,9 +57,9 @@ total_angle = 360*deg;
 %% Calculate spectrum %%
 
 Rot_inc = rotaxi2mat(rot_axis,total_angle/rot_steps);
-Rot_inc_lab = rotaxi2mat([1,0,0],total_angle/rot_steps);
+Rot_inc_lab = rotaxi2mat([1,0,0],-total_angle/rot_steps);
 
-cryst_rot = eye(3);
+cryst_rot = init_rotm;
 cryst_rot_lab = init_rotm;
 x = [];
 y1 = [];
@@ -69,7 +69,7 @@ for n = 0:rot_steps
     angle = n*total_angle/rot_steps;
     %mag_vect_crystal = cryst_rot*init_mag_vect;
     %Exp.CrystalOrientation = eulang(alignMagRot(mag_vect_crystal));
-    Exp.CrystalOrientation = eulang(cryst_rot_lab);
+    Exp.CrystalOrientation = eulang(cryst_rot);
     out = eigfields(Sys,Exp,Opt);
     fields1 = out{1}';
     fields2 = out{2}';
@@ -79,7 +79,7 @@ for n = 0:rot_steps
     y1 = [y1 fields1];
     y2 = [y2 fields2];
     
-    cryst_rot = Rot_inc*cryst_rot;
+    cryst_rot = cryst_rot*Rot_inc;
     cryst_rot_lab = Rot_inc_lab*cryst_rot_lab;
 end
 
