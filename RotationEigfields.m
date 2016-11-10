6dbca06a9ed347f9e62a02a99aa45cbba208a335
Gavin Dold <gdold42@gmail.com>
2016-11-10 13:55:37 +0000

    Code now works for rotations in both crystal frame and lab frame

--- RotationEigfields.m
+++ RotationEigfields.m
@@ -42,13 +42,14 @@ init_mag_vect = [-0.156794;0.987480;-0.017280];
 % init_mag_vect = [0.99998808;-0.004875;-2.358e-04];
 
 % The initial rotation from the crystal frame to the lab frame is then
+init_mag_vect = init_mag_vect/norm(init_mag_vect);
 y_axis_in_cryst = cross(init_mag_vect,rot_axis); % true if rot,mag orthogonal
 init_rotm = [rot_axis,y_axis_in_cryst,init_mag_vect]'; % orthonormal?
 
 
 % Want a threshold intensity below which eigfields will ignore transition?
 Opt = struct();
-%Opt.Threshold = 100;    eulang(cryst_rot_lab)
+%Opt.Threshold = 100;
 
 % Number of steps in rotation simulation
 rot_steps = 72;
@@ -58,6 +59,7 @@ total_angle = 360*deg;
 
 Rot_inc = rotaxi2mat(rot_axis,total_angle/rot_steps);
 Rot_inc_lab = rotaxi2mat([1,0,0],-total_angle/rot_steps);
+Rot_inc_lab = eye(3);
 
 cryst_rot = init_rotm;
 cryst_rot_lab = init_rotm;
@@ -79,8 +81,7 @@ for n = 0:rot_steps
     y1 = [y1 fields1];
     y2 = [y2 fields2];
     
-    cryst_rot = cryst_rot*Rot_inc;
-    cryst_rot_lab = Rot_inc_lab*cryst_rot_lab;
+    cryst_rot = Rot_inc_lab*cryst_rot*Rot_inc'; %Inverse crystal rotation
 end
 
 figure
