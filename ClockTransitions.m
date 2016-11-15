f26d6033aa0e00861fc96cac952cec98217dad22
Gavin Dold <gdold42@gmail.com>
2016-11-15 17:55:54 +0000

    Much neatened code with unnecessary lines removed

--- ClockTransitions.m
+++ ClockTransitions.m
@@ -1,4 +1,5 @@
 deg = pi/180;
+rt = 1.0/sqrt(2);
 
 %% Spin system %%
 Sys = struct();
@@ -6,81 +7,59 @@ Sys.Nucs = '145Nd';
 %Sys.I = 3.5; %Specified by Nucs(?)
 Sys.S = 0.5;
 
-
-%% g and A tensors from source %%
+% g and A tensors from source %
 % Allowed sources:
 % 'Maier-FlaigTensor'
 % 'Maier-FlaigPrincipal' % gives vastly different results to other two
 % 'Wolfowicz' - corrected Euler convention
-%parameter_source = 'Maier-FlaigPrincipal';
 parameter_source = 'Maier-FlaigTensor';
 Sys = NdYSOparams(Sys,parameter_source); % Appends chosen parameters to Sys
-
-% The eigenvaues M-F Tensor do not match up with
-% M-F Principal - is this right?!
 
 %% Experimental parameters %%
 
 Exp = struct();
-%Exp.mwFreq = 9.385; %GHz % 9.7 perhaps?
-Exp.Range = [0 3]; %mT % [350 600]
+Exp.Range = [0 3]; %GHz
 Exp.CrystalSymmetry = 'C2h'; %monoclinic C^6_2h spacegroup 
 Exp.Temperature = 20; %Kelvin
-Exp.Mode = 'perpendicular'; % direction of MW field, 'perpendicular' or 'parallel'
 
-%% Crystal rotation %%
-% Axes defined in crystal basis [D1 D2 b]
-
-magaxis = 'b';
-MWaxis = 'b';
-[init_rotm, Exp.Mode] = setInitialAxes(magaxis,MWaxis); % currently only crystal axes
-
-rot_axis = [0,1,0];
-
-% The initial rotation from the crystal frame to the lab frame is then
-
-
+%% Other simulation parameters %%
 % Want a threshold intensity below which eigfields will ignore transition?
 Opt = struct();
 Opt.Threshold = 0; % Get all transitions (incl forbidden)
-%Opt.Sites = [1,2];
-Opt.Sites = [1];
+Opt.Sites = 1; % [1,2] for both YSO sites
 
+%% Sweep parameters %%
+% Axes defined in crystal basis [D1 D2 b]
 
+% Set up field sweep parameters
+max_field = 50;
+field_steps = 500;
+magaxis = 'D1';
+MWaxis = 'D2';
+[init_rotm, Exp.Mode] = setInitialAxes(magaxis,MWaxis); % currently only crystal axes
+Exp.Mode = [0,90*deg];
 
-%% Calculate spectrum %%
-
-
+% Set up rotation parameters
+rotaxis = 'D1'; % set this manually
+total_angle = 360*deg;
+rot_steps = 0; % 0 for no angular sweep
+rot_axis = [1,0,0]; % crystal frame
+rot_axis_lab = [1,0,0]; % lab frame
 
 cryst_rot = init_rotm;
 cryst_rot_lab = init_rotm;
-x = [];
-y = [];
-freqs = [];
-
-Exp.Field = 20; % mT % WANT TO SWEEP MW FIELD
-Exp.mwFreq = 2.0; % GHz
-threshold = 0.1*0.01;
-Opt.Threshold = 0;
-
-% Set up field sweep
-max_field = 50;
-field_steps = 500;
-
-% Set up rotation
-total_angle = 360*deg;
-rot_steps = 0; % 0 for no angular sweep
-
 Rot_inc = rotaxi2mat(rot_axis,total_angle/rot_steps);
-%Rot_inc_lab = rotaxi2mat([1,0,0],-total_angle/rot_steps);
+%Rot_inc_lab = rotaxi2mat(rot_axis_lab,-total_angle/rot_steps);
 Rot_inc_lab = eye(3);
 
+%% Run sweep %%
 
+% Full struct contains all dat structs from rotation sweep
 full_empty = struct();
 full_empty.data = struct();
 full_empty.angle = [];
 
-
+% Dat struct contains data from each field sweep
 dat_empty = struct();
 NaNarray = repelem(NaN,field_steps+1,1); % preallocate memory for vects
 dat_empty.transition = [];
@@ -107,14 +86,10 @@ for step = 0:rot_steps
     Site = [];
     
     dat = dat_empty();
-    %dat.angle = angle;
-    
-    
     
     for n = 0:field_steps
-        freqs = [];
-        %disp(n);
         mag_field = n*max_field/field_steps;
+        %disp([int2str(mag_field),'mT'])
         Exp.Field = mag_field;
         Exp.CrystalOrientation = eulang(cryst_rot);
         [Pos,Amp,~,Trans] = resfreqs_matrix(Sys,Exp,Opt);
@@ -125,11 +100,9 @@ for step = 0:rot_steps
         end
         transition_label = Site(:)*10000 + Trans(:,1)*100 + Trans(:,2);% label x->y by int xxyy, works only if <100 transitions
         field = repelem(mag_field,length(Pos))';
-        %out = [field,Pos,Amp,transition_label,Site];% label is cast to double
         
         dat(1).transition = [1 2]; % prevent duplicate...
         dat(1).label = 10102; % lazy hack...
-        
         for i = 1:length(Trans)
             [in,loc] = ismember(transition_label(i),[dat(:).label]);
             if ~in
@@ -157,28 +130,19 @@ for step = 0:rot_steps
     z = z(:);
     
     %figure
-    rot_axis_str = 'D2';
     
-    %hold off
-    %scatter(x,y,'.')
+
     scatter(x,y,[],z,'.')
     colormap(flipud(hot))
     cbar = colorbar();
     cbar.Label.String = 'Amplitude';
     %caxis([0.0,1.0])
-    ylim([0,3000])
-    %hold on
-    %scatter(x,y2,'.')
+    ylim(Exp.Range*1000)
     xlabel('B (mT)')
     ylabel('Transition frequency (MHz)')
-    %title(['Mag vector: ',num2str(init_mag_vect')])
-    title(['Mag axis: ',magaxis,'; MW axis: ',MWaxis,'; Rot axis: ',rot_axis_str,'; angle: ',num2str(angle/deg)])
-    %tit = 'TEST';
-    %title(tit);
+    title(['Mag axis: ',magaxis,'; MW axis: ',MWaxis,'; Rot axis: ',rotaxis,'; angle: ',num2str(angle/deg)])
     findClockTransitions;
     %saveas(gcf,['figure',int2str(5*step),'.png'])
-    %saveas(gcf,[tit,'.png'])
-    %save(['output',int2str(5*step),'.mat'],'output')
     %text_label = {['Source: ',parameter_source],['Rotation axis: ',num2str(rot_axis')]};
     %annotation('textbox',[.2 .5 .3 .3],'string',text_label,'FitBoxToText','on');
     
